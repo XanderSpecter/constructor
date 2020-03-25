ad3b2e92369f675d4fce39faebae06f16aec5f31
XanderSpecter <sbogdanov1993@gmail.com>
2020-03-25 22:32:49 +0500

    test app

---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    color: red;
+    display: flex;
+`;
+
+const App = () => <Button>123</Button>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = (env, argv = {}) => {
+    const isProd = argv.mode === 'production';
+
+    const config = {
+        entry: {
+            app: './src/index.tsx',
+        },
+        output: {
+            path: path.join(__dirname, './dist'),
+            publicPath: '/',
+            filename: '[name].[hash].js',
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin(),
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    },
+                },
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                files: {
+                    css: ['styles.[hash].css'],
+                    js: ['app.[hash].js'],
+                },
+                template: './src/index.html',
+                filename: './index.html',
+                inject: false,
+            }),
+        ]
+    };
+
+    return config;
+};
